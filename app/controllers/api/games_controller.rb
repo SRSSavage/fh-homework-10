03fc4d07a714b2dec392a8b7637d90768cd89c91
Zach Hewitt <zach@zachs-mbp.home>
2019-10-03 17:56:09 -0600

    added private methods

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -3,5 +3,17 @@ module API
     def index
       render json: Game.all
     end
+
+
+
+    private
+
+    def set_game
+      @game = Game.find_by(id: params[:id])
+    end
+
+    def game_params
+      require(:game).permit(:name, :home_team_id, :away_team_id)
+    end
   end
 end
