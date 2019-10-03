b497a43e45191086a02a2ccc319352fc8fae3427
Zach Hewitt <zach@zachs-mbp.home>
2019-10-03 16:45:24 -0600

    added show method to pass spec

--- app/controllers/api/teams_controller.rb
+++ app/controllers/api/teams_controller.rb
@@ -3,5 +3,25 @@ module API
     def index 
       render json: Team.all
     end
+
+    def show
+      team = set_team
+      if team
+        render json: team, status: 200
+      else
+        render json: team, status: 422
+      end
+    end
+
+
+    private
+
+    def set_team
+      @team = Team.find_by(id: params[:id])
+    end
+
+    def team_params
+      params.require(:team).permit(:name)
+    end
   end
 end
