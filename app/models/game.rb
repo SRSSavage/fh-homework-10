9915f5815a595ba1d1beb45ed63b0db176f499bb
Zach Hewitt <zach@Zachs-MacBook-Pro.local>
2019-10-10 15:26:33 -0600

    removed foreign keys

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ApplicationRecord
-  belongs_to :home_team, class_name: 'Team', foreign_key: :"away_team_id"
-  belongs_to :away_team, class_name: 'Team', foreign_key: :"away_team_id"
+  belongs_to :home_team, class_name: 'Team'
+  belongs_to :away_team, class_name: 'Team'
 
   validates :name, presence: true, length: { minimum: 1 }
   validates :away_team_id, presence: true, numericality: { only_integer: true }
