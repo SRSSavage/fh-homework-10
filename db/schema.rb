eaf4fc0c4535a479496582deea17d85220162041
Zach Hewitt <zach@Zachs-MacBook-Pro.local>
2019-11-07 13:49:30 -0700

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_14_235458) do
+ActiveRecord::Schema.define(version: 2019_10_24_205206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,9 +19,11 @@ ActiveRecord::Schema.define(version: 2019_10_14_235458) do
     t.string "name"
     t.integer "home_team_id"
     t.integer "away_team_id"
+    t.integer "team_id"
     t.datetime "played_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["team_id"], name: "index_games_on_team_id"
   end
 
   create_table "players", force: :cascade do |t|
@@ -32,15 +34,25 @@ ActiveRecord::Schema.define(version: 2019_10_14_235458) do
     t.string "position"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "team_id"
-    t.index ["team_id"], name: "index_players_on_team_id"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.index ["email"], name: "index_players_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_players_on_reset_password_token", unique: true
   end
 
   create_table "teams", force: :cascade do |t|
     t.string "name"
+    t.integer "player_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["player_id"], name: "index_teams_on_player_id"
   end
 
-  add_foreign_key "players", "teams"
 end
